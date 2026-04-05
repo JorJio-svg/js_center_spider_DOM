ee35de6b288f1924a68aefe5815691322cd5cf84
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-04-05 15:29:32 +0300

    make build

---
+++ index.c0919662.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientHeight,l=e.clientWidth;t.style.top="".concat(c/2,"px"),t.style.left="".concat(l/2,"px");
+//# sourceMappingURL=index.c0919662.js.map
